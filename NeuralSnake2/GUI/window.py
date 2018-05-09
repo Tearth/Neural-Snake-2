17f67b0df5c93bdd15a2abdec14fd95a7b2ca818
tearth <Tearth@users.noreply.github.com>
2018-05-10 01:03:54 +0200

    Add canvas to the window where board will be drawn.

--- NeuralSnake2/GUI/window.py
+++ NeuralSnake2/GUI/window.py
@@ -5,6 +5,10 @@ class Window(object):
         self._root = Tk()
         self._root.title("NeuralNetwork 2")
         self._root.geometry("600x400")
+        self._root.resizable(False, False)
+
+        self._canvas = Canvas(self._root, width=400, height=400, bg="white")
+        self._canvas.pack(side="left")
 
     def run(self):
         self._root.mainloop()
--- NeuralSnake2/core.py
+++ NeuralSnake2/core.py
@@ -1,8 +1,10 @@
 from window import *
+from board import *
 
 class Core(object):
     def __init__(self):
         self._window = Window()
+        self._board = Board(10, 10);
 
     def run(self):
         self._window.run()
