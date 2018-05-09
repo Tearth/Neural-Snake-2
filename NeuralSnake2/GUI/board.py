6b5402522d9fef425d4795fb82518a0f0b8082c2
tearth <Tearth@users.noreply.github.com>
2018-05-10 01:03:21 +0200

    Add Board class.

---
+++ NeuralSnake2/GUI/board.py
@@ -0,0 +1,10 @@
+from field import *
+
+class Board(object):
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+        self._fields = [Field.NONE] * width
+        for i in range(self._width):
+            self._fields[i] = [Field.NONE] * height
---
+++ NeuralSnake2/GUI/field.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Field(Enum):
+    NONE = 0,
+    HEAD = 1,
+    BODY = 2,
+    FOOD = 3,
+    WALL = 4
