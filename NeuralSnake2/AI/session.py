a742a6e7cbab268f8b132ddd2b871ba6e4302471
tearth <Tearth@users.noreply.github.com>
2018-05-13 18:28:18 +0200

    Add genotype decoding.

--- NeuralSnake2/AI/session.py
+++ NeuralSnake2/AI/session.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from game import *
 from numpy import array
 
@@ -7,31 +8,23 @@ class Session(object):
         self._game = Game()
         self.initNetwork()
 
-    def initNetwork(self):
+    def initNetwork(self, genotype = None):
         self._input =  tf.placeholder("float", [None, Constants.INPUT_NEURONS_COUNT],  name="input_data")
         self._output = tf.placeholder("float", [None, Constants.OUTPUT_NEURONS_COUNT], name="output_data")
         
         self._lastInput = []
         self._lastOutput = []
-
-        self._weights = {
-            'input_weights':  tf.Variable(tf.random_normal([Constants.INPUT_NEURONS_COUNT,  Constants.INPUT_NEURONS_COUNT]),  name="input_weights"),
-            'hidden_weights': tf.Variable(tf.random_normal([Constants.INPUT_NEURONS_COUNT,  Constants.HIDDEN_NEURONS_COUNT]), name="hidden_weights"),
-            'output_weights': tf.Variable(tf.random_normal([Constants.HIDDEN_NEURONS_COUNT, Constants.OUTPUT_NEURONS_COUNT]), name="output_weights")
-        }
-
-        self._biases = {
-            'input_biases':  tf.Variable(tf.random_normal([Constants.INPUT_NEURONS_COUNT]),  name="input_weights"),
-            'hidden_biases': tf.Variable(tf.random_normal([Constants.HIDDEN_NEURONS_COUNT]), name="hidden_biases"),
-            'output_biases': tf.Variable(tf.random_normal([Constants.OUTPUT_NEURONS_COUNT]), name="output_biases")
-        }
+        
+        if genotype == None:
+            self._initWeightAndBiases()
+        else:
+            self._initWeightAndBiasesWithGenotype(genotype)
 
         self._inputActivationFunction  = tf.nn.tanh((tf.matmul(self._input,                    self._weights['input_weights'])  + self._biases['input_biases']),  name="input_act_func")
         self._hiddenActivationFunction = tf.nn.tanh((tf.matmul(self._inputActivationFunction,  self._weights['hidden_weights']) + self._biases['hidden_biases']), name="hidden_act_func")
         self._outputActivationFunction = tf.nn.tanh((tf.matmul(self._hiddenActivationFunction, self._weights['output_weights']) + self._biases['output_biases']), name="output_act_func")
 
-        self._tfSession = tf.Session() 
-        self._tfSession.run(tf.global_variables_initializer())
+        self._tfSession = tf.Session()
 
     def nextTurn(self):
         input_values = []
@@ -64,3 +57,55 @@ class Session(object):
 
     def isRunning(self):
         return self._game.running
+
+    def _initWeightAndBiases(self):
+        self._genotype = [0] * self._getGenotypeLength()
+        for i in range(len(self._genotype)):
+            self._genotype[i] = uniform(-1, 1)
+
+        self._initWeightAndBiasesWithGenotype(self._genotype)
+
+    def _initWeightAndBiasesWithGenotype(self, genotype):
+        self._weights = {
+            'input_weights':  tf.Variable(tf.zeros([Constants.INPUT_NEURONS_COUNT,  Constants.INPUT_NEURONS_COUNT]),  name="input_weights"),
+            'hidden_weights': tf.Variable(tf.zeros([Constants.INPUT_NEURONS_COUNT,  Constants.HIDDEN_NEURONS_COUNT]), name="hidden_weights"),
+            'output_weights': tf.Variable(tf.zeros([Constants.HIDDEN_NEURONS_COUNT, Constants.OUTPUT_NEURONS_COUNT]), name="output_weights")
+        }
+
+        self._biases = {
+            'input_biases':  tf.Variable(tf.zeros([Constants.INPUT_NEURONS_COUNT]),  name="input_weights"),
+            'hidden_biases': tf.Variable(tf.zeros([Constants.HIDDEN_NEURONS_COUNT]), name="hidden_biases"),
+            'output_biases': tf.Variable(tf.zeros([Constants.OUTPUT_NEURONS_COUNT]), name="output_biases")
+        }
+
+        input_weights = self._genotype[0:Constants.INPUT_NEURONS_COUNT * Constants.INPUT_NEURONS_COUNT]
+        length = len(input_weights)
+
+        hidden_weights = self._genotype[length:length + Constants.INPUT_NEURONS_COUNT * Constants.HIDDEN_NEURONS_COUNT]
+        length += len(hidden_weights)
+
+        output_weights = self._genotype[length:length + Constants.HIDDEN_NEURONS_COUNT * Constants.OUTPUT_NEURONS_COUNT]
+        length += len(output_weights)
+
+        input_biases = self._genotype[length:length + Constants.INPUT_NEURONS_COUNT]
+        length += len(input_biases)
+
+        hidden_biases = self._genotype[length:length + Constants.HIDDEN_NEURONS_COUNT]
+        length += len(hidden_biases)
+
+        output_biases = self._genotype[length:length + Constants.OUTPUT_NEURONS_COUNT]
+        length += len(output_biases)
+
+        self._weights['input_weights'] = tf.assign(self._weights['input_weights'], array(input_weights).reshape(Constants.INPUT_NEURONS_COUNT, Constants.INPUT_NEURONS_COUNT))
+        self._weights['hidden_weights'] = tf.assign(self._weights['hidden_weights'], array(hidden_weights).reshape(Constants.INPUT_NEURONS_COUNT, Constants.HIDDEN_NEURONS_COUNT))
+        self._weights['output_weights'] = tf.assign(self._weights['output_weights'], array(output_weights).reshape(Constants.HIDDEN_NEURONS_COUNT, Constants.OUTPUT_NEURONS_COUNT))
+
+        self._biases['input_biases'] = tf.assign(self._biases['input_biases'], input_biases)
+        self._biases['hidden_biases'] = tf.assign(self._biases['hidden_biases'], hidden_biases)
+        self._biases['output_biases'] = tf.assign(self._biases['output_biases'], output_biases)
+
+    def _getGenotypeLength(self):
+        return (Constants.INPUT_NEURONS_COUNT * Constants.INPUT_NEURONS_COUNT) + \
+               (Constants.INPUT_NEURONS_COUNT * Constants.HIDDEN_NEURONS_COUNT) + \
+               (Constants.HIDDEN_NEURONS_COUNT * Constants.OUTPUT_NEURONS_COUNT) + \
+               (Constants.INPUT_NEURONS_COUNT + Constants.HIDDEN_NEURONS_COUNT + Constants.OUTPUT_NEURONS_COUNT)
